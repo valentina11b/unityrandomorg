3ec54736537d9cc76cd2d4ce8bb9bf5a246c1bd8
Valentina <valentina.bujic@gmail.com>
2024-08-09 00:58:16 +0200

    Implement all functions in unity, with simple UI

--- UnityProjectBasicApi/Assets/NativeCodeRunnerScript.cs
+++ UnityProjectBasicApi/Assets/NativeCodeRunnerScript.cs
@@ -3,17 +3,64 @@ using System.Collections.Generic;
 using UnityEngine;
 using UnityEngine.UI;
 using TMPro;
+using System;
+using System.Text;
 
 public class NativeCodeRunner : MonoBehaviour {
 
-    [SerializeField] public TMP_Text messageText; // Reference to the Text component you want to show
-    public string message = "Button Pressed!"; // Message to display
+    [SerializeField] public TMP_Text messageText; 
+    public string message = "";
+
+    [SerializeField] public Button runButton;
 
     public void OnButtonPress()
     {
-      string msg = "Number: " + (GetRandomInteger());
-        messageText.text = msg;
-        messageText.enabled = true; // Ensure the text is enabled and visible
+        runButton.enabled = false;
+
+        string msg = "Result:\n";
+        try
+        {
+        if (m_Dropdown.value == 0)
+            {
+                msg += string.Join("\n", GetRandomInteger());
+            }
+            else if (m_Dropdown.value == 1)
+            {
+                int[][] matrix = GetRandomIntegerSequences(5, 5);
+                for (int i = 0; i < 5; i++)
+                {
+                    msg += "[" + string.Join(", ", matrix[i]) + "]\n";
+                }
+
+
+        }
+            else if (m_Dropdown.value == 2)
+            {
+                msg += string.Join("\n", GetRandomDecimalFractions());
+            }
+            else if (m_Dropdown.value == 3)
+            {
+                msg += string.Join("\n", GetRandomGaussians());
+            }
+            else if (m_Dropdown.value == 4)
+            {
+                msg += string.Join("\n", GetRandomStrings());
+            }
+            else if (m_Dropdown.value == 5)
+            {
+                msg += string.Join("\n", GetRandomUUIDs(5));
+            }
+            else if (m_Dropdown.value == 6)
+            {
+                msg += string.Join("\n", GetRandomBlobs());
+            }
+            } catch(Exception e)
+            {
+            msg += e.Message;
+            }
+            messageText.text = msg;
+        messageText.enabled = true;
+        runButton.enabled = true;
     }
 
     [SerializeField] public TMP_Dropdown m_Dropdown;
@@ -26,15 +73,131 @@ public class NativeCodeRunner : MonoBehaviour {
     void Start() {
     } 
 
-     public float GetRandomInteger() {
+     public int[] GetRandomInteger() {
        if (Application.platform == RuntimePlatform.Android) {
            AndroidJavaClass unityPlayerClass = new AndroidJavaClass("com.unity3d.player.UnityPlayer");
            AndroidJavaObject unityActivity = unityPlayerClass.GetStatic<AndroidJavaObject>("currentActivity");        
            AndroidJavaObject alert = new AndroidJavaObject("rs.assignments.basicapi.RandomORGApp");            
-           object[] parameters = new object[1];
+           object[] parameters = new object[4];
            parameters[0] = unityActivity;
-           return alert.Call<float>("GetRandomInteger", parameters);
+           parameters[1] = 5; // n
+           parameters[2] = 0; // min
+           parameters[3] = 55; // max
+           return alert.Call<int[]>("GetRandomInteger", parameters);
        }
-       return -1f;
-     }
+       return new int[0];
+    }
+
+    public int[][] GetRandomIntegerSequences(int n, int len)
+    {
+        if (Application.platform == RuntimePlatform.Android)
+        {
+            AndroidJavaClass unityPlayerClass = new AndroidJavaClass("com.unity3d.player.UnityPlayer");
+            AndroidJavaObject unityActivity = unityPlayerClass.GetStatic<AndroidJavaObject>("currentActivity");
+            AndroidJavaObject alert = new AndroidJavaObject("rs.assignments.basicapi.RandomORGApp");
+            object[] parameters = new object[5];
+            parameters[0] = unityActivity;
+            parameters[1] = n; // n
+            parameters[2] = len; // len
+            parameters[3] = 0; // min
+            parameters[4] = 55; // max
+            int[] ret = alert.Call<int[]>("GetRandomIntegerSequences", parameters);
+            int[][] split = new int[n][];
+            for (int i = 0; i < n; i++)
+            {
+                split[i] = new int[len];
+                for (int j = 0; j < len; j++)
+                {
+                    split[i][j] = ret[i * n + j];
+                }
+            }
+            return split;
+        }
+        return new int[0][];
+    }
+
+    public double[] GetRandomDecimalFractions()
+    {
+        if (Application.platform == RuntimePlatform.Android)
+        {
+            AndroidJavaClass unityPlayerClass = new AndroidJavaClass("com.unity3d.player.UnityPlayer");
+            AndroidJavaObject unityActivity = unityPlayerClass.GetStatic<AndroidJavaObject>("currentActivity");
+            AndroidJavaObject alert = new AndroidJavaObject("rs.assignments.basicapi.RandomORGApp");
+            object[] parameters = new object[3];
+            parameters[0] = unityActivity;
+            parameters[1] = 5; // n
+            parameters[2] = 3; // decimalPlaces
+            return alert.Call<double[]>("GetRandomDecimalFractions", parameters);
+        }
+        return new double[0];
+    }
+
+    public double[] GetRandomGaussians()
+    {
+        if (Application.platform == RuntimePlatform.Android)
+        {
+            AndroidJavaClass unityPlayerClass = new AndroidJavaClass("com.unity3d.player.UnityPlayer");
+            AndroidJavaObject unityActivity = unityPlayerClass.GetStatic<AndroidJavaObject>("currentActivity");
+            AndroidJavaObject alert = new AndroidJavaObject("rs.assignments.basicapi.RandomORGApp");
+            object[] parameters = new object[5];
+            parameters[0] = unityActivity;
+            parameters[1] = 4; // n
+            parameters[2] = 0.0; // mean
+            parameters[3] = 1.0; // standardDeviation
+            parameters[4] = 8; // significantDigits
+            return alert.Call<double[]>("GetRandomGaussians", parameters);
+        }
+        return new double[0];
+    }
+    public string[] GetRandomStrings()
+    {
+        if (Application.platform == RuntimePlatform.Android)
+        {
+            AndroidJavaClass unityPlayerClass = new AndroidJavaClass("com.unity3d.player.UnityPlayer");
+            AndroidJavaObject unityActivity = unityPlayerClass.GetStatic<AndroidJavaObject>("currentActivity");
+            AndroidJavaObject alert = new AndroidJavaObject("rs.assignments.basicapi.RandomORGApp");
+            object[] parameters = new object[4];
+            parameters[0] = unityActivity;
+            parameters[1] = 4; // n
+            parameters[2] = 9; // len
+            parameters[3] = "abcdefghijklmnopqrstuvwxyz"; // characters
+            return alert.Call<string[]>("GetRandomStrings", parameters);
+        }
+        return new string[0];
+    }
+    public Guid[] GetRandomUUIDs(int n)
+    {
+        if (Application.platform == RuntimePlatform.Android)
+        {
+            AndroidJavaClass unityPlayerClass = new AndroidJavaClass("com.unity3d.player.UnityPlayer");
+            AndroidJavaObject unityActivity = unityPlayerClass.GetStatic<AndroidJavaObject>("currentActivity");
+            AndroidJavaObject alert = new AndroidJavaObject("rs.assignments.basicapi.RandomORGApp");
+            object[] parameters = new object[2];
+            parameters[0] = unityActivity;
+            parameters[1] = n;
+            string[] strs = alert.Call<string[]>("GetRandomUUIDs", parameters);
+            Guid[] guids = new Guid[n];
+            for (int i = 0; i < n; i++)
+            {
+                guids[i] = new Guid(strs[i]);
+            }
+            return guids;
+        }
+        return new Guid[0];
+    }
+    public string[] GetRandomBlobs()
+    {
+        if (Application.platform == RuntimePlatform.Android)
+        {
+            AndroidJavaClass unityPlayerClass = new AndroidJavaClass("com.unity3d.player.UnityPlayer");
+            AndroidJavaObject unityActivity = unityPlayerClass.GetStatic<AndroidJavaObject>("currentActivity");
+            AndroidJavaObject alert = new AndroidJavaObject("rs.assignments.basicapi.RandomORGApp");
+            object[] parameters = new object[3];
+            parameters[0] = unityActivity;
+            parameters[1] = 4; // n
+            parameters[2] = 8; // size, must be divisible by 8
+            return alert.Call<string[]>("GetRandomBlobs", parameters);
+        }
+        return new string[0];
+    }
 }
--- randomorgbasicapi/basicapi/src/main/java/rs/assignments/basicapi/RandomORGApp.java
+++ randomorgbasicapi/basicapi/src/main/java/rs/assignments/basicapi/RandomORGApp.java
@@ -1,20 +1,97 @@
 package rs.assignments.basicapi;
+
 import android.app.Application;
 import android.content.Context;
 
 import java.io.IOException;
+import java.util.UUID;
 
 public class RandomORGApp extends Application {
 
-    public float GetRandomInteger(Context context) {
+    public int[] GetRandomInteger(Context context, int n, int min, int max) {
         String apiKey = context.getString(R.string.api_key);
         org.random.api.RandomOrgClient roc = org.random.api.RandomOrgClient.getRandomOrgClient(apiKey);
 
         try {
-            int[] randoms = roc.generateIntegers(5, 0, 10);
-            return randoms[0];
+            return roc.generateIntegers(n, min, max);
         } catch (IOException e) {
-            return -1;
+            return new int[0];
+        }
+    }
+
+    public int[] GetRandomIntegerSequences(Context context, int n, int len, int min, int max) {
+        String apiKey = context.getString(R.string.api_key);
+        org.random.api.RandomOrgClient roc = org.random.api.RandomOrgClient.getRandomOrgClient(apiKey);
+
+        try {
+            int[][] res = roc.generateIntegerSequences(n, len, min, max);
+            int[] ret = new int[n*len];
+            for(int i = 0; i < n; i++) {
+                if (len >= 0) System.arraycopy(res[i], 0, ret, i * n, len);
+            }
+            return ret;
+        } catch (IOException e) {
+            return new int[0];
+        }
+    }
+
+    public double[] GetRandomDecimalFractions(Context context, int n, int decimalPlaces) {
+        String apiKey = context.getString(R.string.api_key);
+        org.random.api.RandomOrgClient roc = org.random.api.RandomOrgClient.getRandomOrgClient(apiKey);
+
+        try {
+            return roc.generateDecimalFractions(n, decimalPlaces);
+        } catch (IOException e) {
+            return new double[0];
+        }
+    }
+
+    public double[] GetRandomGaussians(Context context, int n, double mean, double standardDeviation, int significantDigits) {
+        String apiKey = context.getString(R.string.api_key);
+        org.random.api.RandomOrgClient roc = org.random.api.RandomOrgClient.getRandomOrgClient(apiKey);
+
+        try {
+            return roc.generateGaussians(n, mean, standardDeviation, significantDigits);
+        } catch (IOException e) {
+            return new double[0];
+        }
+    }
+
+    public String[] GetRandomStrings(Context context, int n, int len, String characters) {
+        String apiKey = context.getString(R.string.api_key);
+        org.random.api.RandomOrgClient roc = org.random.api.RandomOrgClient.getRandomOrgClient(apiKey);
+
+        try {
+            return roc.generateStrings(n, len, characters);
+        } catch (IOException e) {
+            return new String[0];
+        }
+    }
+
+    public String[] GetRandomUUIDs(Context context, int n) {
+        String apiKey = context.getString(R.string.api_key);
+        org.random.api.RandomOrgClient roc = org.random.api.RandomOrgClient.getRandomOrgClient(apiKey);
+
+        try {
+            UUID[] uuids = roc.generateUUIDs(n);
+            String[] strs = new String[uuids.length];
+            for (int i = 0; i < uuids.length; i++) {
+                strs[i] = uuids[i].toString();
+            }
+            return strs;
+        } catch (IOException e) {
+            return new String[0];
+        }
+    }
+
+    public String[] GetRandomBlobs(Context context, int n, int size) {
+        String apiKey = context.getString(R.string.api_key);
+        org.random.api.RandomOrgClient roc = org.random.api.RandomOrgClient.getRandomOrgClient(apiKey);
+
+        try {
+            return roc.generateBlobs(n, size);
+        } catch (IOException e) {
+            return new String[0];
         }
     }
 }
